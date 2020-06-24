e271fc3ed244f1c31d9d846f47d5dbd86868ad7a
Maria Gabriela Cuenca <mariagabitsi@gmail.com>
2020-06-24 15:41:57 -0300

    add a classe dao e validação dos dados

--- e-digi/src/controller/Autor.java
+++ e-digi/src/controller/Autor.java
@@ -1,11 +1,9 @@
 package controller;
 
-
 public class Autor {
 
     private String nome;
     private String email;
-
 
     public Autor(String nome, String email) {
         setNome(nome);
@@ -17,8 +15,9 @@ public class Autor {
     }
 
     public void setNome(String nome) {
+        // valida campo em branco ou se apenas tem espaço.
         if(nome.equals(" ") || nome.equals("")){
-            throw new IllegalArgumentException("Não é permitido nomes em branco, por favor digite um nome válido.");
+            throw new IllegalArgumentException("Não é permitido nomes em branco, por favor digite um nome válido!");
         }else{
             this.nome = nome;
         }
@@ -30,6 +29,7 @@ public class Autor {
     }
 
     public void setEmail(String email) {
+        // valida se o valor digitado é um email
         String regex = "^(.+)@(.+)$";
         
         if(email.matches(regex)){
@@ -39,5 +39,26 @@ public class Autor {
         }
  
     }
+
+    // método para sobrescrever o hash para que permita criar um objeto do mesmo tipo
+    @Override
+  public int hashCode() {
+    return this.email.charAt(0);
+  }
   
+  // método para sobrescrever o equals, para comparar se tem email duplicado
+  @Override 
+  public boolean equals(Object obj) {
+    Autor novo = (Autor) obj;
+    return this.email.equals(novo.email);
+  }
+
+
+    @Override
+    public String toString() {
+        return "Nome: " + getNome() + "\n" +
+            "Email: " + getEmail();
+    }
+
+
 }
