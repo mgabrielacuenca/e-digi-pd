23b53f95d8183469fe831a9215452cb3df3671b2
Maria Gabriela Cuenca <mariagabitsi@gmail.com>
2020-06-30 09:56:27 -0300

    ajustes das classes principais

--- e-digi/src/controller/MainCategoria.java
+++ e-digi/src/controller/MainCategoria.java
@@ -1,19 +1,9 @@
 package controller;
 
-import model.DaoAutor;
 import model.DaoCategoria;
 
-public class Main {
+public class MainCategoria {
     public static void main(final String[] args) throws Exception {
-
-        // criando autor
-        Autor autor = new Autor("Gabriela", "mariagabitsi@gmail.com");
-        Autor autor1 = new Autor("Gabriela", "mariagabi@gmail.com");
-
-        // realizando as inserções e imprimindo no caso de sucesso
-        DaoAutor autoradd = new DaoAutor();
-        autoradd.addAutor(autor);
-        autoradd.addAutor(autor1);
 
         // criando categoria
         Categoria categoria = new Categoria("Suspense");
